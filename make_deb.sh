0543bf83e760d7af4da87abd02a1e0a2db9f0a43
Marco Vito Moscaritolo <mavimo@gmail.com>
2009-03-08 16:20:06 +0100

    Add *.deb packaging generation script

---
+++ make_deb.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+# Remove old ./build/tmp folder
+rm -rf ./build/tmp
+
+# Configure control folder and file
+mkdir -p ./build/tmp/drupal-snippet
+cp -r DEBIAN ./build/tmp/drupal-snippet/
+
+# Copy language specs
+mkdir -p ./build/tmp/drupal-snippet/usr/share/gtksourceview-2.0/language-specs
+cp ./drupal.lang ./build/tmp/drupal-snippet/usr/share/gtksourceview-2.0/language-specs/drupal.lang
+
+# Copy mime type
+mkdir -p ./build/tmp/drupal-snippet/usr/share/mime/packages
+cp ./drupal-mime.xml ./build/tmp/drupal-snippet/usr/share/mime/packages/druapl.mime.xml
+
+# Copy Snippets
+mkdir -p ./build/tmp/gdrupal-snippet/usr/share/gedit-2/gmate/snippets
+cp -r ./drupal.all.xml ./build/tmp/drupal-snippet/usr/share/gedit-2/snippets/drupal.xml
+
+# Copy icons
+mkdir -p ./build/tmp/drupal-snippet/usr/share/icons/gnome/scalable/mimetype/
+cp application-x-drupal-php.svg ./build/tmp/drupal-snippet/usr/share/icons/gnome/scalable/mimetype/application-x-drupal-php.svg
+
+# Copy documentation
+mkdir -p ./build/tmp/drupal-snippet/usr/share/gedit-2/drupal-snippet/doc
+cp README ./build/tmp/drupal-snippet/usr/share/gedit-2/drupal-snippet/doc/
+
+# Make the deb package
+dpkg-deb -b ./build/tmp/drupal-snippet .
+
+# Remove temporary directory
+rm -rf ./build
