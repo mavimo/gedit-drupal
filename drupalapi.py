518032c1b43dd3e2a59b1aea4a6a25b7af7735c5
mavimo <mavimo@gmail.com>
2011-03-19 12:51:06 +0100

    fixed #13 by dennis605: using external window to open reference page

--- drupalapi.py
+++ drupalapi.py
@@ -6,36 +6,6 @@ import webkit
 import types
 
 class DrupalApi(gedit.Plugin):
-  # A list of (handler_id, view) tuples
-  handler_ids = []
-
-  def __init__(self):
-    # Create Drupal API browser
-    self._panel = webkit.WebView()
-    # = gtkmozembed.MozEmbed()
-    gedit.Plugin.__init__(self)
-
-  def activate(self, window):
-    # Load image icon
-    image = gtk.image_new_from_file(os.path.join(os.path.dirname(__file__), 'drupalapi.png'))
-    
-    # Find bottm panel obj
-    bottom = window.get_bottom_panel()
-    
-    # Add new item
-    bottom.add_item(self._panel, _("Drupal API"), image)
-    
-    # Start auto completion for the active view
-    view = window.get_active_view()
-    self.setup_dapi_autocompletion(view)
-
-  def deactivate(self, window):
-    # Remove browser into bottom Panel
-    bottom = window.get_bottom_panel()
-    bottom.remove_item(self._panel)
-    # Disconnect all handlers which have been connected by this plugin
-    for (handler_id, view) in self.handler_ids:
-      view.disconnect(handler_id)
 
   def update_ui(self, window):
     # Start auto completion for the active view
@@ -46,18 +16,12 @@ class DrupalApi(gedit.Plugin):
   def setup_dapi_autocompletion(self, view):
     if type(view) != types.NoneType:
       if getattr(view, 'completion_instance', False) == False:
-        setattr(view, 'completion_instance', DApiCompletion(self._panel))
+        setattr(view, 'completion_instance', DApiCompletion())
         handler_id = view.connect(
           'key-press-event',
           view.completion_instance.complete_word)
-        view.completion_instance.handler_id = handler_id
-        self.handler_ids.append((handler_id, view))
 
 class DApiCompletion():
-  def __init__(self, panel):
-    self._dapi_panel = panel
-    return
-
   def _current_module_name(self):
     window = gedit.app_get_default().get_active_window()
     doc = window.get_active_document()
@@ -76,17 +40,19 @@ class DApiCompletion():
       return True
     if(char == ','):
       return True
+    if(char == '.'):
+      return True
     
     return False
 
   def complete_word(self, view, event):
     # Use multiple mouse click on function to open API Doc 
     # (change 'key-press-event' to 'button-press-event')
+    #
     #    if (event.type == gtk.gdk._3BUTTON_PRESS):
-    # Used to find Keycode to use
-    #    print event.keyval
-    # Set CTRL + KEYCODE to open API Doc
-    if(event.state and gtk.gdk.CONTROL_MASK and gtk.gdk.keyval_name(event.keyval) == 'l'):
+    #
+    # Set CTRL + CHAR to open API Doc
+    if(event.state & gtk.gdk.CONTROL_MASK and gtk.gdk.keyval_name(event.keyval) == 'l'):
       buffer      = view.get_buffer()
       iter_cursor = buffer.get_iter_at_mark(buffer.get_insert())
       iter_start  = iter_cursor.copy()
@@ -109,10 +75,25 @@ class DApiCompletion():
       # if start with same file name is a hook
       if(function.startswith(name + '_')):
         function = function.replace(name + '_', 'hook_', 1)
-      
+
       # Load info page
-      self._dapi_panel.open('http://api.drupal.org/api/function/' + function)
+      # Window elements
+      main_window     = gtk.Window ()
+      scrolled_window = gtk.ScrolledWindow ()
+      web_view        = webkit.WebView ()
+      # Add elements into window
+      gtk.Container.add (scrolled_window, web_view)
+      gtk.Container.add (main_window, scrolled_window)
+      # Set URL to open
+      webkit.WebView.load_uri (web_view, "http://api.drupal.org/api/function/" + function + '/6')
+      # Set window size
+      gtk.Window.set_default_size (main_window, 1024, 600)
+      # Set window position (center)
+      main_window.set_position(gtk.WIN_POS_CENTER)
+      # Set window title
+      main_window.set_title ("Drupal 6 API: " + function)
+      # Show window
+      gtk.Widget.show_all (main_window)
       return False
     else:
       return False
-
